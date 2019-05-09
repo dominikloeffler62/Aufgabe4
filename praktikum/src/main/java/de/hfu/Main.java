5ba17d3d479f019d1da1981aa00598a451d95d9d
Dominik Loeffler <dominik.loeffler@hs-furtwangen.de>
2019-05-09 14:37:12 +0200

    update

--- praktikum/src/main/java/de/hfu/Main.java
+++ praktikum/src/main/java/de/hfu/Main.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 
+	/**Die Main Methode*/
 	public static void main(String[] args) {
 		
-		//Scanner wird benutzt für die Eingabe
 		String eingabe;
 		Scanner scan = new Scanner(System.in);
 		
 		eingabe = scan.nextLine();
 		
-		//Hier wird Alles Groß
 		eingabe  = eingabe.toUpperCase();
 		
-		//Hier wird die Ausgabe wieder ausgegeben
 		System.out.println(eingabe);
 
 	}
